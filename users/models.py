f90ce6cfdd9c456dc4271ef8c249c3a4782a0e9c
Manish Khandar <manishkhandar13@gmail.com>
2023-07-04 17:59:23 +0530

    Implemented Api endpoint for User, Implemented Redis caching mechanism

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=False)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(primary_key=True)
+    password = models.CharField(max_length=100)
---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import UserDetailView
+
+urlpatterns = [
+    path('', UserDetailView.as_view()), # create
+    path('<int:user_id>/', UserDetailView.as_view()), # get , update
+]
